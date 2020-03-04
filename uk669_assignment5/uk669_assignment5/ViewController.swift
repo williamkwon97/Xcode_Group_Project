8c664002563f44b663e1d811f260ec2abf5471df
William Kwon <williamkwon97@gmail.com>
2020-03-04 13:08:39 -0600

    First commit

---
+++ uk669_assignment5/uk669_assignment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uk669_assignment5
+//
+//  Created by William Kwon on 3/4/20.
+//  Copyright © 2020 William Kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
